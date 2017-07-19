ae171dc338bd79933c88a975af416ebc37e70ae7
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-07-19 11:54:25 +0300

    'resource' is not a valid type hint

--- lib/Utils.php
+++ lib/Utils.php
@@ -4,17 +4,11 @@ namespace WildWolf;
 
 abstract class Utils
 {
-    public static function safe_fopen(string $fname, string $mode, bool $use_include_path = false, resource $context = null)
+    public static function safe_fopen(string $fname, string $mode, bool $use_include_path = false, $context = null)
     {
         $level = error_reporting();
         error_reporting($level & ~E_WARNING);
-        if ($context === null) {
-            $retval = fopen($fname, $mode, $use_include_path);
-        }
-        else {
-            $retval = fopen($fname, $mode, $use_include_path, $context);
-        }
-
+        $retval = fopen($fname, $mode, $use_include_path, $context);
         error_reporting($level);
         return $retval;
     }
