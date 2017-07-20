6081b5746d1d88afdb7bed4bd37afe4a9e50414b
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-07-20 17:38:50 +0300

    LogOut Handler test

---
+++ tests/unit/LogOutHandlerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace WildWolf\Test;
+
+class LogOutHandlerTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var \WildWolf\Handler\LogOut
+     */
+    private $handler;
+
+    /**
+     * @var \Slim\Slim
+     */
+    private $app;
+
+    protected function setUp()
+    {
+        $this->app = new \Slim\Slim();
+
+        $acckit = $this->createMock(\WildWolf\AccountKit::class);
+        $acckit->method('logout')->willReturn(true);
+        $this->app->container->singleton('acckit', function() use ($acckit) { return $acckit; });
+
+        $this->handler = new \WildWolf\Handler\LogOut($this->app);
+    }
+
+    protected function tearDown()
+    {
+        $_SESSION = [];
+    }
+
+    public function testNotLoggedInUser()
+    {
+        try {
+            ($this->handler)();
+            $this->assertTrue(false);
+        }
+        catch (\Slim\Exception\Stop $e) {
+            $this->assertFalse(isset($_SESSION['user']));
+
+            $response = $this->app->response;
+            $this->assertTrue($response->headers()->has('Location'));
+
+            $location = $response->headers()->get('Location');
+            $this->assertEquals('/', $location);
+        }
+    }
+
+    public function testLoggedInUser()
+    {
+        $_SESSION = [
+            'user' => (object)[
+                'token' => 'sometoken',
+            ]
+        ];
+
+        try {
+            ($this->handler)();
+            $this->assertTrue(false);
+        }
+        catch (\Slim\Exception\Stop $e) {
+            $this->assertFalse(isset($_SESSION['user']));
+
+            $response = $this->app->response;
+            $this->assertTrue($response->headers()->has('Location'));
+
+            $location = $response->headers()->get('Location');
+            $this->assertEquals('/', $location);
+        }
+    }
+}
