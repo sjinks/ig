6b2b7035039b186b57c693dbd62f03a5a0769ab8
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-08-26 22:40:21 +0300

    Force 404 status

--- lib/Handler/NotFound.php
+++ lib/Handler/NotFound.php
@@ -6,6 +6,6 @@ class NotFound extends BaseHandler
 {
     protected function run()
     {
-        $this->app->render('404.phtml');
+        $this->app->render('404.phtml', ['title' => 'Не знайдено'], 404);
     }
 }
